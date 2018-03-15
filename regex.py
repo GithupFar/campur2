b85d668ed6c0ffa0d46172b38b0673c8e1ed4538
Rahandi Noor Pasha <rahandi16@mhs.if.its.ac.id>
2018-03-15 16:19:41 +0700

    Add files via upload

--- regex.py
+++ regex.py
@@ -2,16 +2,57 @@ import re
 
 class regex_checker():
     def __init__(self, regexnya):
+        if regexnya == '':
+            regexnya = '(a+b)*aa(a+b)*bb(a+b)*+(a+b)*bb(a+b)*aa(a+b)*'
         regexnya = regexnya.replace('+', '|')
         regexnya = regexnya.replace(' ', '')
         self.checker = re.compile(regexnya)
 
     def cek(self, stringnya):
         data = self.checker.match(stringnya)
-        if(data.group() == stringnya):
-            return True
+        if(data == None):
+            return False, stringnya
+        elif(data.group() == stringnya):
+            return True, data.group()
         else:
-            return False
+            return False, data.group()
+
+    def state(self, current, inputnya):
+        state_diagram = {
+            'q0': {
+                'a': 'q1',
+                'b': 'q2'
+            },
+            'q1': {
+                'a': 'q3',
+                'b': 'q2'
+            },
+            'q2': {
+                'a': 'q1',
+                'b': 'q4'
+            },
+            'q3': {
+                'a': 'q3',
+                'b': 'q5'
+            },
+            'q4': {
+                'a': 'q6',
+                'b': 'q4'
+            },
+            'q5': {
+                'a': 'q3',
+                'b': 'q7'
+            },
+            'q6': {
+                'a': 'q7',
+                'b': 'q4'
+            },
+            'q7': {
+                'a': 'q7',
+                'b': 'q7'
+            }
+        }
+        return state_diagram[current][inputnya]
 
 if __name__ == '__main__':
     reg = raw_input("Masukkan RE: ")
@@ -20,4 +61,15 @@ if __name__ == '__main__':
         inputan = raw_input("Masukkan string: ")
         if inputan.lower() == "exit":
             exit()
-        print regex.cek(inputan)
+        hasil, bahan = regex.cek(inputan)
+        print hasil
+        if bool(bahan) == False:
+            print 'q0'
+        else:
+            state = regex.state('q0', bahan[0])
+            total_state = state + ' '
+            bahan = bahan[1:]
+            for b in bahan:
+                state = regex.state(state, b)
+                total_state += state + ' '
+            print total_state
